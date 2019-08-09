ab7b66417aff52bcc2af750b85a435e71e246eb6
garybot <argot.pryer@gmail.com>
2019-08-08 21:25:13 -0700

    Completed closure stretch goals.'

--- assignments/closure.js
+++ assignments/closure.js
@@ -24,14 +24,36 @@ console.log(addFriend("Travis"));
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return () => ++count;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
-
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
+console.log(newCounter()); 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return {
+  	increment: function() {
+  		return ++count;
+  	},
+  	decrement: function() {
+  		return --count;
+  	}
+  }
 };
+
+
+const newCountFact = counterFactory();
+
+console.log(newCountFact.increment());
+console.log(newCountFact.increment());
+console.log(newCountFact.increment());
+console.log(newCountFact.decrement());
+console.log(newCountFact.decrement());
+
+console.log(newCountFact.increment());
